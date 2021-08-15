3ffa9d46b28f4fb8715e54a7b99e3ed72a7f9f66
elalelopez <elalelopez@gmail.com>
2021-08-15 02:33:43 -0300

    Modify CustomSearchEngine for support string search + get sellers products by distance

--- modules/advancedsearch/classes/CustomSearchEngine.php
+++ modules/advancedsearch/classes/CustomSearchEngine.php
@@ -8,17 +8,67 @@ use PrestaShop\PrestaShop\Core\Product\Search\ProductSearchResult;
 class CustomSearchEngine implements ProductSearchProviderInterface{
 
     protected $products;
+    protected $string;
 
-    public function __construct($params)
+    public function __construct($params,$string)
     {
-        $this->products = $params["products"];
+        if($params != null && $string != false){
+            $this->string = $string;
+            $product = [];
+            foreach($params as $param){
+                foreach($param as $prod){
+                    $product[] = $prod;
+                }
+            }
+            $this->products = $product;
+        }
+        else{
+            $this->products = [];
+        }
     }
 
     public function runQuery(ProductSearchContext $context, ProductSearchQuery $query){
-        var_dump($this->products);die();
+
+        $products = [];
+        $count = 0;
+        $query->setSearchString(true);
+
+        if (($string = $query->getSearchString())) {
+            $queryString = Tools::replaceAccentedChars(urldecode($this->string));
+
+            $result = Search::find(
+                $context->getIdLang(),
+                $queryString,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false, // ajax, what's the link?
+                false, // $use_cookie, ignored anyway
+                null
+            );
+            $products = $result['result'];
+            $count = $result['total'];
+
+            Hook::exec('actionSearch', [
+                'searched_query' => $queryString,
+                'total' => $count,
+            ]);
+        }
+
+        $prods = [];
+        foreach($products as $product){
+            $prds["id_product"] = $product["id_product"];
+            $prods[] = $prds;
+        }
+
+        $sellerprods = array_intersect($this->products,$prods);
+
         $new_products = new ProductSearchResult();
-        $array_list = array(array('id_product' => 23),array('id_product' => 25));
-        $new_products->setProducts($array_list);
+        if (!empty($this->products)) {
+            $array_list = $sellerprods;
+            $new_products->setProducts($array_list);
+        }
         return $new_products;
     }
 }
