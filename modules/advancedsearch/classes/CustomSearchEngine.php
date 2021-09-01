c420d99d9f52036aa01d5e3f0b0296686f852aba
elalelopez <elalelopez@gmail.com>
2021-09-01 00:00:32 -0300

    Fix native search

--- modules/advancedsearch/classes/CustomSearchEngine.php
+++ modules/advancedsearch/classes/CustomSearchEngine.php
@@ -23,6 +23,7 @@ class CustomSearchEngine implements ProductSearchProviderInterface
             $this->products = $product;
         } else {
             $this->products = [];
+            $this->string = null;
         }
     }
 
@@ -37,55 +38,115 @@ class CustomSearchEngine implements ProductSearchProviderInterface
 
         $products = [];
         $count = 0;
-        $query->setSearchString($this->string);
-
-        $queryString = Tools::replaceAccentedChars(urldecode($this->string));
-
-        $result = Search::find(
-            $context->getIdLang(),
-            $queryString,
-            $query->getPage(),
-            $query->getResultsPerPage(),
-            $query->getSortOrder()->toLegacyOrderBy(),
-            $query->getSortOrder()->toLegacyOrderWay(),
-            false, // ajax, what's the link?
-            false, // $use_cookie, ignored anyway
-            null
-        );
-
-        $products = $result['result'];
-        $count = $result['total'];
-
-        Hook::exec('actionSearch', [ 
-            'searched_query' => $queryString,
-            'total' => $count, 
-            // deprecated since 1.7.x
-            'expr' => $queryString,
-        ]);
         
-        $prods = [];
-        foreach ($products as $product) {
-            $prods[] = $product["id_product"];
+        //$queryString = Tools::replaceAccentedChars(urldecode($this->string));
+
+        if ($this->string != null){
+            $query->setSearchString($this->string);
+        }
+        
+        if (($string = $query->getSearchString())) {
+            $queryString = Tools::replaceAccentedChars(urldecode($string));
+
+            $result = Search::find(
+                $context->getIdLang(),
+                $queryString,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false, // ajax, what's the link?
+                false, // $use_cookie, ignored anyway
+                null
+            );
+
+            $products = $result['result'];
+            $count = $result['total'];
+
+            Hook::exec('actionSearch', [ 
+                'searched_query' => $queryString,
+                'total' => $count, 
+                // deprecated since 1.7.x
+                'expr' => $queryString,
+            ]);
+        
+        } elseif (($tag = $query->getSearchTag())) {
+            $queryString = urldecode($tag);
+
+            $products = Search::searchTag(
+                $context->getIdLang(),
+                $queryString,
+                false,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(true),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false,
+                null
+            );
+
+            $count = Search::searchTag(
+                $context->getIdLang(),
+                $queryString,
+                true,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(true),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false,
+                null
+            );
+
+            Hook::exec('actionSearch', [
+                'searched_query' => $queryString,
+                'total' => $count,
+
+                // deprecated since 1.7.x
+                'expr' => $queryString,
+            ]);
         }
 
-        $prodd = [];
-        foreach ($this->products as $product) {
-            $prodd[] = $product["id_product"];
+        if (is_null($this->string)){
+
+            $result = new ProductSearchResult();
+
+            if (!empty($products)) {
+                $result
+                    ->setProducts($products)
+                    ->setTotalProductsCount($count);
+
+               // $result->setAvailableSortOrders(
+               //     $this->sortOrderFactory->getDefaultSortOrders()
+               // );
+            }
+        } else {
+        
+            $prods = [];
+            foreach ($products as $product) {
+                $prods[] = $product["id_product"];
+            }
+
+            $prodd = [];
+            foreach ($this->products as $product) {
+                $prodd[] = $product["id_product"];
+            }
+
+            $sellerprods = array_intersect($prods,$prodd);
+
+            $array_prods = [];
+            foreach ($sellerprods as $prod) {
+                $aprod['id_product'] = $prod;
+                $array_prods[] = $aprod; 
+            }
+
+            $new_products = new ProductSearchResult();
+            if (!empty($array_prods)) {
+                $new_products->setProducts($array_prods);
+            }
+
+            $result = $new_products;
         }
 
-        $sellerprods = array_intersect($prods,$prodd);
-
-        $array_prods = [];
-        foreach ($sellerprods as $prod) {
-            $aprod['id_product'] = $prod;
-            $array_prods[] = $aprod; 
-        }
-
-        $new_products = new ProductSearchResult();
-        if (!empty($array_prods)) {
-            $new_products->setProducts($array_prods);
-        }
-
-        return $new_products;
+        return $result;
     }
 }
